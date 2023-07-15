477bb98eb94728b55e62b05f816c43c27523556c
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-15 12:35:43 -0300

    TodoList, TaskItem and NewTask css completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import styles from "./App.module.css";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
 import { Header } from "./components/Header";
 import { NewTask } from "./components/NewTask";
 import { TodoList } from "./components/TodoList";
 
+import styles from "./App.module.css";
+
 export function App() {
+
+
   return (
     <>
       <Header />
--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,16 +1,38 @@
-import { CheckCircle, Trash } from "phosphor-react";
+import { Trash } from "phosphor-react";
 
 import styles from "./TaskItem.module.css";
+import { useState } from "react";
 
 export function TaskItem() {
+  const [isChecked, setIsChecked] = useState(false);
+  
+  function handleToggleTaskCompletion() {
+    setIsChecked(!isChecked);
+  }
+
+  console.log(isChecked);
+
   return (
     <div className={styles.taskItemContainer}>
-      <CheckCircle />
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
-        iure.
-      </span>
-      <Trash />
+      <div className={styles.taskItemLeft}>
+        <div className={styles.taskItemCheckboxContainer}>
+          <input 
+            defaultChecked={isChecked}
+            type="checkbox" 
+            id="checkbox1" 
+            onClick={() => handleToggleTaskCompletion()}
+          />
+          <label htmlFor="checkbox1"/>
+        </div>
+        <span className={!isChecked ? styles.taskItemLeftSpan : styles.taskItemLeftSpanUnderlined}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+          corrupti harum vitae laudantium culpa corporis incidunt quis provident
+          magnam mollitia!
+        </span>
+      </div>
+      <div className={styles.taskItemRight}>
+        <Trash />
+      </div>
     </div>
   );
 }
